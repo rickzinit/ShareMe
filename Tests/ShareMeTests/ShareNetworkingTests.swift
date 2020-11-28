8e06d6c2f31e05fc647533fb77d3960bdeb6b34c
Rick Camus <rick.camus@commitent.com>
2020-11-28 10:05:50 -0500

    Add first networking API for retrieving network data

--- Tests/ShareMeTests/ShareNetworkingTests.swift
+++ Tests/ShareMeTests/ShareNetworkingTests.swift
@@ -30,14 +30,27 @@ import XCTest
 @testable import ShareMe
 
 final class ShareNetworkingTests: XCTestCase {
+    func testLoadDataCall() {
+        let manager = Share.Networking.Manager()
+        let expectation = XCTestExpectation(description: "Called for data")
+        guard let url = URL(string: "https://raywenderlich.com") else {
+            return XCTFail("Could not create URL properly")
+        }
+        manager.loadData(from: url) { result in
+            expectation.fulfill()
+            switch result {
+                case .success(let returnedData):
+                    XCTAssertNotNil (returnedData, "Response data is nil")
+                case .failure(let error):
+                    XCTFail(error?.localizedDescription ?? "error forming error result")
+            }
+        }
+        wait(for: [expectation], timeout: 5)
+    }
   
-  func testExample() {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-  }
   
   static var allTests = [
-    ("testExample", testExample),
+    ("testLoadDataCall", testLoadDataCall),
   ]
   
 }
